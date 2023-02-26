f3ef954324bc04960d6d52ee09fa10349b51379a
Kalidass Mookkaiah <kalidass@gmail.com>
2023-02-26 02:14:30 +0000

    Print the http request in server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-use std::net::TcpListener;
+use std::{
+    net::{TcpListener, TcpStream},
+    io::{prelude::*, BufReader},
+};
 
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listner.incoming(){
-        let _streams= stream.unwrap();
+        let streams= stream.unwrap();
 
-        println!("Connection established!!")
+        handle_connections(streams)
     }
+}
+
+fn handle_connections(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result |result.unwrap())
+        .take_while(|line | !line.is_empty())
+        .collect();
+
+    println!("request: {:#?}", http_request)
 }
